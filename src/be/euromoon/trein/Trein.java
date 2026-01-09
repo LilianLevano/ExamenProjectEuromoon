ad596db3390b1418468f07f17bc2d3d99312ed6b
Lilian Levano <zey.lilianlevano@gmail.com>
2026-01-09 11:24:16 +0100

    Trein basis functies en fields gemaakt, relatie tussen Trein en TypeLocomotief gelegd

--- src/be/euromoon/trein/Trein.java
+++ src/be/euromoon/trein/Trein.java
@@ -1,4 +1,44 @@
 package be.euromoon.trein;
 
 public class Trein {
+
+    private TypeLocomotief typeLocomotief;
+    private int aantalZitplaatsen;
+
+    public Trein(TypeLocomotief typeLocomotief) {
+        this.typeLocomotief = typeLocomotief;
+
+        if (typeLocomotief == TypeLocomotief.CLASS_373){
+            aantalZitplaatsen = 80*typeLocomotief.getMaxAantalWagon();
+        }else if(typeLocomotief == TypeLocomotief.CLASS_374){
+            aantalZitplaatsen = 80*typeLocomotief.getMaxAantalWagon();
+        }
+
+    }
+
+    public TypeLocomotief getTypeLocomotief() {
+        return typeLocomotief;
+    }
+
+    public void setTypeLocomotief(TypeLocomotief typeLocomotief) {
+        this.typeLocomotief = typeLocomotief;
+
+        if (typeLocomotief == TypeLocomotief.CLASS_373){
+            aantalZitplaatsen = 80*typeLocomotief.getMaxAantalWagon();
+        }else if(typeLocomotief == TypeLocomotief.CLASS_374){
+            aantalZitplaatsen = 80*typeLocomotief.getMaxAantalWagon();
+        }
+    }
+
+    public int getAantalZitplaatsen() {
+        return aantalZitplaatsen;
+    }
+
+    @Override
+    public String toString() {
+        return "Trein{" +
+                "typeLocomotief = " + typeLocomotief +
+                ", aantalZitplaatsen = " + aantalZitplaatsen +
+                '}';
+    }
 }
