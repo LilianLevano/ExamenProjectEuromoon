a18e7c6f172cecb51c83764245c861a5bd2e440b
Lilian Levano <zey.lilianlevano@gmail.com>
2026-01-09 11:18:29 +0100

    Enum TypeLocomotief gemaakt

--- src/be/euromoon/trein/TypeLocomotief.java
+++ src/be/euromoon/trein/TypeLocomotief.java
@@ -1,4 +1,16 @@
 package be.euromoon.trein;
 
 public enum TypeLocomotief {
+
+    CLASS_373(12), CLASS_374(14);
+
+    private int maxAantalWagon;
+
+    TypeLocomotief(int maxAantalWagon) {
+        this.maxAantalWagon = maxAantalWagon;
+    }
+
+    public int getMaxAantalWagon() {
+        return maxAantalWagon;
+    }
 }
